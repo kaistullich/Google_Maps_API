cf90c39ff0f680cdbb1369d9e3d0f290cad7dc69
kaistullich <kairstullich@gmail.com>
2017-02-05 22:35:03 -0800

    created formatted address variable

--- google_maps_get.py
+++ google_maps_get.py
@@ -3,8 +3,10 @@ import requests
 
 
 google_url = 'http://maps.googleapis.com/maps/api/geocode/json?'
-address = 'lhr'
+address = 'morgan hill'
 url = google_url + urllib.parse.urlencode({'address': address})
-google_json_data = requests.get(url).json() . # creates json data from the requests 
+google_json_data = requests.get(url).json()  # creates json data from the requests
 
 return_status = google_json_data['status']  # returns the status message
+formatted_address = google_json_data['results'][0]['formatted_address']
+print(formatted_address)
