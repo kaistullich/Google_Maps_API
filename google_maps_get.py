9cc9ecd868432944ad57caf26de045960233824f
kaistullich <kairstullich@gmail.com>
2017-02-05 22:32:15 -0800

    grammar

--- google_maps_get.py
+++ google_maps_get.py
@@ -5,6 +5,6 @@ import requests
 google_url = 'http://maps.googleapis.com/maps/api/geocode/json?'
 address = 'lhr'
 url = google_url + urllib.parse.urlencode({'address': address})
-google_json_data = requests.get(url).json() . # creates json dats from the requests 
+google_json_data = requests.get(url).json() . # creates json data from the requests 
 
 return_status = google_json_data['status']  # returns the status message
