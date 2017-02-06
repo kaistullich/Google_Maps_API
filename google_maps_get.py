ef9559842ae121d46f62857f399a54fd8873d258
kaistullich <kairstullich@gmail.com>
2017-02-05 22:45:25 -0800

    changed formatted address varibale

--- google_maps_get.py
+++ google_maps_get.py
@@ -8,7 +8,7 @@ url = google_url + urllib.parse.urlencode({'address': address})
 google_json_data = requests.get(url).json()  # creates json data from the requests
 
 return_status = google_json_data['status']  # returns the status message
-formatted_address = google_json_data['results'][0]['formatted_address']
+full_address = google_json_data['results'][0]['formatted_address']
 addr_compo_city_long_name = google_json_data['results'][0]['address_components'][0]['long_name']
 addr_compo_county_long_name = google_json_data['results'][0]['address_components'][1]['long_name']
 addr_compo_state_long_name = google_json_data['results'][0]['address_components'][2]['long_name']
