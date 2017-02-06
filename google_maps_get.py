ac634ed2322a0a7471725138b78986efca7d98cb
kaistullich <kairstullich@gmail.com>
2017-02-05 22:51:32 -0800

    variables moved in order from reponse

--- google_maps_get.py
+++ google_maps_get.py
@@ -7,11 +7,11 @@ address = 'morgan hill'
 url = google_url + urllib.parse.urlencode({'address': address})
 google_json_data = requests.get(url).json()  # creates json data from the requests
 
-return_status = google_json_data['status']  # returns the status message
-full_address = google_json_data['results'][0]['formatted_address']
 addr_compo_city_long_name = google_json_data['results'][0]['address_components'][0]['long_name']
 addr_compo_county_long_name = google_json_data['results'][0]['address_components'][1]['long_name']
 addr_compo_state_long_name = google_json_data['results'][0]['address_components'][2]['long_name']
 addr_compo_state_short_name = google_json_data['results'][0]['address_components'][2]['short_name']
+full_address = google_json_data['results'][0]['formatted_address']
 geo_lng = google_json_data['results'][0]['geometry']['location']['lng']
 geo_lat = google_json_data['results'][0]['geometry']['location']['lat']
+return_status = google_json_data['status']  # returns the status message
