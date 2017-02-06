f25857a4afedb239fa0673d4a609b2040228d3af
kaistullich <kairstullich@gmail.com>
2017-02-05 22:31:22 -0800

    changed variable names for more clarity

--- google_maps_get.py
+++ google_maps_get.py
@@ -5,6 +5,6 @@ import requests
 google_url = 'http://maps.googleapis.com/maps/api/geocode/json?'
 address = 'lhr'
 url = google_url + urllib.parse.urlencode({'address': address})
-json_data = requests.get(url).json() . # creates json dats from the requests 
+google_json_data = requests.get(url).json() . # creates json dats from the requests 
 
-return_status = json_data['status']  # returns the status message
+return_status = google_json_data['status']  # returns the status message
