b930f1f9820d28135bcae89d60517f1fe3b934f1
kaistullich <kairstullich@gmail.com>
2017-02-06 22:25:30 -0800

    assigned variable to function for return

--- google_maps_get.py
+++ google_maps_get.py
@@ -11,8 +11,7 @@ while True:
         url = google_url + urllib.parse.urlencode({'address': user_input})
         print(url)
         google_json_data = requests.get(url).json()  # creates json data from the requests
-
-        addr_compo_state_short_name = google_json_data['results'][0]['address_components'][2]['short_name']  # i.e. CA
+        
         full_address = google_json_data['results'][0]['formatted_address']  # i.e. (Morgan Hill, CA, USA)
         geo_lng = google_json_data['results'][0]['geometry']['location']['lng']  # returns longitude
         geo_lat = google_json_data['results'][0]['geometry']['location']['lat']  # returns latitude
@@ -32,24 +31,15 @@ while True:
             del short_name_addr[-1]
             
             return long_name_addr, short_name_addr
-                
-        x = address_info()
-        print(x)
+            
+        long_short_addr = address_info()
         
         print('\nInformation pertaining to your search:\n')
-          
-        ''' Uncomment all lines you want information for '''
-
-		address_info()
-		print('\tFull address: ' + full_address)
-		print('\tCity: ' + addr_compo_city_long_name)
-		print('\tCounty: ' + addr_compo_county_long_name)
-		print('\tState/Country: ' + addr_compo_state_long_name)
-		print('\tState/Country abreviation: ' + addr_compo_state_short_name)
-		print('\tLongitude: ' + str(geo_lng))
-		print('\tLatitude: ' + str(geo_lat))
-		print('\tRequest Status: ' + return_status)
-		print('-' * 50)
+        print('\tFull address: ' + full_address)
+        print('\tLongitude: ' + str(geo_lng))
+        print('\tLatitude: ' + str(geo_lat))
+        print('\tRequest Status: ' + return_status)
+        print('-' * 50)
         
     except Exception as e:
         print('\n** An unexpected error occured. Please try again! **\n')
